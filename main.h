6c4d053ef196f7b4a5d96c5ed67a324ca409aba1
RigzTeche <fredrickarara@gmail.com>
2023-11-08 01:08:45 -0800

    main.h

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <string.h>
+
+
